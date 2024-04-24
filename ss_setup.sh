9ca6422c919bce58f7f5913b8824b5ba4361741a
buffalobillhuang <78633608+buffalobillhuang@users.noreply.github.com>
2024-04-25 02:24:36 +0800

    Add files via upload

---
+++ ss_setup.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python3-pip
+sudo pip3 install https://github.com/shadowsocks/shadowsocks/archive/master.zip
+sudo ssserver --version
+sudo vim /etc/shadowsocks.json
+{
+    "server":"0.0.0.0",
+    "server_port":443,
+    "local_port":1080,
+    "password":"Beijing100",
+    "timeout":300,
+    "method":"aes-256-cfb"
+}
+:wq!
+sudo /usr/local/bin/ssserver -c /etc/shadowsocks.json -d start
